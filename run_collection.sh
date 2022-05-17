fe9cf8798626b8a5db1c82f140e77cd0659af72e
xavierpuigf <xavierpuig@csail.mit.edu>
2022-05-16 22:34:14 -0400

    update

---
+++ run_collection.sh
@@ -0,0 +1,2 @@
+export EXEC="/data/vision/torralba/frames/data_acquisition/SyntheticStories/website/release/simulator/v2.0/online_wah/online_wah_exec_2/linux_exec.online_wah_2.x86_64"
+python vh_demo.py --deployment remote --execname $EXEC --task_group 3 311 420 --extra_agent B1 B1 B1
